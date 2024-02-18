f3581f37b34efe455bcdaaf7dc954868a8d994da
Matilde Sardinha <matildefialho@gmail.com>
2024-02-18 23:47:05 +0000

    added stuff

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+#define regUDP "59000"
+#define regIDP "193.136.138.142"
+
+int main (int argc, char **argv)
+{
+
+}
